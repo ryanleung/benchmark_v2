0209cf806f9dc7db8b3b98df4dc83878babc0242
Ryan Leung <ryanleung@berkeley.edu>
2017-05-28 19:02:59 -0700

    Persist logged in user and fix logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
-    resources :sessions, only: [:create, :destroy, :show]
+    resources :sessions, only: [:create, :show]
+    delete 'sessions' => 'sessions#destroy'
   end
   root "static_pages#root"
 end

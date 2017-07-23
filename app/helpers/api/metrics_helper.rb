9bdb73433da213adc427b9ee837c6434c1e30273
Ryan Leung <ryanleung@berkeley.edu>
2017-07-23 01:18:23 -0700

    Restructure metrics dashboard format

--- app/helpers/api/metrics_helper.rb
+++ app/helpers/api/metrics_helper.rb
@@ -6,14 +6,38 @@ module Api::MetricsHelper
   # Take a company, poop out their metrics dashboard.
   def metrics_dashboard(company_id)
     {
-      accounts_per_sales_rep: accounts_per_sales_rep(company_id),
-      annual_revenue: annual_revenue(company_id),
-      direct_sales_reps_per_1k_fte: direct_sales_reps_per_1k_fte(company_id),
-      #overall_sales_fte_role_breakdown: overall_sales_fte_role_breakdown(company_id),
-      overall_sales_per_1k_fte: overall_sales_reps_per_1k_fte(company_id),
-      revenue_per_employee: revenue_per_employee(company_id),
-      sales_support_per_1k_fte: sales_support_per_1k_fte(company_id),
-      total_num_employees: total_num_employees(company_id),
+      overview: {
+        annual_revenue: {
+          title: "Revenue",
+          metrics: annual_revenue(company_id),
+        },
+        total_num_employees: {
+          title: "Employees",
+          metrics: total_num_employees(company_id),
+        },
+        revenue_per_employee: {
+          title: "Revenue Per Employee",
+          metrics: revenue_per_employee(company_id),
+        },
+      },
+      sales_organization_structure: {
+        overall_sales_per_1k_fte: {
+          title: "Overall Sales per FTE",
+          metrics: overall_sales_reps_per_1k_fte(company_id),
+        },
+        direct_sales_reps_per_1k_fte: {
+          title: "Direct Sales Reps FTE per 1000 FTE",
+          metrics: direct_sales_reps_per_1k_fte(company_id),
+        },
+        accounts_per_sales_rep: {
+          title: "Accounts per Sales Rep",
+          metrics: accounts_per_sales_rep(company_id),
+        },
+        sales_support_per_1k_fte: {
+          title: "Sales Support FTE per 1000 FTE",
+          metrics: sales_support_per_1k_fte(company_id),
+        }
+      }
     }
   end
   module_function :metrics_dashboard

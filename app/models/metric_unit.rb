b3feb2291112b3427fbb278b9955b246140f29ce
Ryan Leung <ryanleung@berkeley.edu>
2017-08-06 23:43:00 -0700

    Add metric unit class

---
+++ app/models/metric_unit.rb
@@ -0,0 +1,14 @@
+class MetricUnit
+  # A Metric Unit is a unit that consists of one or more metrics that
+  # show up on the metrics dashboard. Each metric is an individual
+  # datapoint submitted by a user. A metric unit is an entire
+  # presentable metric to a customer. A customer can purchase
+  # metric units.
+  MU_REVENUE = "mu_revenue"
+  MU_EMPLOYEES = "mu_employees"
+  MU_REVENUE_PER_EMPLOYEE = "mu_revenue_per_employee"
+  MU_OVERALL_SALES_PER_FTE = "mu_overall_sales_per_fte"
+  MU_DIRECT_SALES_PER_FTE = "mu_direct_sales_per_fte"
+  MU_ACCOUNTS_PER_SALES_REP = "mu_accounts_per_sales_rep"
+  MU_SALES_SUPPORT_PER_FTE = "mu_sales_support_per_fte"
+end

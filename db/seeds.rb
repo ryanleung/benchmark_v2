f788c9ec0d965e479e1049220a92d84fcaaa2dd7
Ryan Leung <ryanleung@berkeley.edu>
2017-10-29 18:09:25 -0700

    Move script to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -307,4 +307,28 @@ ActiveRecord::Base.transaction do
                          value_description: Metric::VALUE_DESC_USD,
                          geo: "US",
                          relevant_date: Date.parse('19-03-2017')
+
+  csv_file = File.join(Rails.root, "lib", "resources", "Company Screening Report.xls - Screening.csv")
+  ActiveRecord::Base.transaction do
+    CSV.foreach(csv_file, :headers => true) do |row|
+      puts row
+      puts '\n'
+      company_name_with_ticker = row["Company Name"]
+      ticker = row["Exchange:Ticker"]
+      industry_type = row["Industry Classifications"]
+      website_url = row["Website"]
+      primary_address = row["Primary Address"]
+
+      # 1. Strip company names of their ticker information (e.g. 2U, Inc. (NasdaqGS:TWOU))
+      company_name = company_name_with_ticker.split("(")[0].strip
+
+      # 2. Get logo url from Clearbit API
+      # TODO: in the future, store in our AWS bucket
+      image_url = "https://logo.clearbit.com/#{website_url}?size=150&format=png"
+
+      # 3. Save company in DB
+      # TODO: chnage industry id
+      company = Company.create name: company_name, logo_img_url: image_url, industry_id: 1
+    end
+  end
 end

b7bb1feef083947650ece8d3816e32a735d544f9
ryanleung <ryanleung@berkeley.edu>
2017-11-19 02:33:27 -0800

    Stylize login form (#14)

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { browserHistory } from 'react-router'
+import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
+import { withStyles } from 'material-ui/styles';
+
+
+const styles = theme => ({
+  loginFormContainer: {
+    display: 'flex',
+    width: 1000,
+    backgroundColor: 'white',
+    margin: [0, "auto"],
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  input: {
+    display: 'flex',
+    verticalAlign: 'middle',
+    lineHeight: theme.spacing.unit / 3,
+    whiteSpace: 'normal',
+    marginBottom: 10,
+  },
+})
 
 class SessionForm extends React.Component {
   constructor(props) {
@@ -54,36 +81,43 @@ class SessionForm extends React.Component {
   }
 
   render() {
+    const { classes } = this.props
+
     return (
-      <div className="login-form-container">
-        <form onSubmit={this.handleSubmit} className="login-form-box">
-          Welcome to Benchmark!
-          <br/>
-          Please {this.props.formType} or {this.navLink()}
+      <div className={classes.loginFormContainer}>
+        <h1>Welcome to Benchmark!</h1>
+        <h2>Please {this.props.formType} or {this.navLink()}</h2>
+        <form onSubmit={this.handleSubmit} className={classes.form}>
           {this.renderErrors()}
-          <div className="login-form">
-            <br/>
-            <label> Email:
-              <input type="text"
-                value={this.state.email}
-                onChange={this.updateField("email")}
-                className="login-input" />
-            </label>
-            <br/>
-            <label> Password:
-              <input type="password"
-                value={this.state.password}
-                onChange={this.updateField("password")}
-                className="login-input" />
-            </label>
-            <br/>
-            <input type="submit" value="Submit" />
-          </div>
+          <TextField
+            className={classes.input}
+            onKeyPress={(ev) => {
+              if (ev.key === 'Enter') {
+                this.handleSubmit(ev)
+              }
+            }}
+            placeholder={"Email"}
+            onChange={this.updateField("email")}
+            value={this.state.email}/>
+          <TextField
+            className={classes.input}
+            onKeyPress={(ev) => {
+              if (ev.key === 'Enter') {
+                this.handleSubmit(ev)
+              }
+            }}
+            placeholder={"Password"}
+            onChange={this.updateField("password")}
+            value={this.state.password}
+            type="password"/>
         </form>
+        <Button raised color="primary" className={classes.button} onClick={this.handleSubmit}>
+          Submit
+        </Button>
       </div>
     );
   }
 
 }
 
-export default withRouter(SessionForm);
+export default withStyles(styles)(withRouter(SessionForm));

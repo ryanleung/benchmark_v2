a3acbefdfb35af9727f2ea912d4ae60c4f1fdb7e
Ryan Leung <ryanleung@berkeley.edu>
2017-11-18 23:14:31 -0800

    Explore page revamp

--- frontend/components/company/company_table_view.jsx
+++ frontend/components/company/company_table_view.jsx
@@ -1,7 +1,9 @@
 import React, { Component, PropTypes } from 'react'
-import { withStyles } from 'material-ui/styles';
+import { withRouter } from  'react-router-dom'
+import { browserHistory } from 'react-router'
+import { withStyles } from 'material-ui/styles'
 import Avatar from 'material-ui/Avatar'
-import Table, { TableBody, TableCell, TableRow } from 'material-ui/Table';
+import Table, { TableBody, TableCell, TableRow } from 'material-ui/Table'
 
 import Company from '../../models/Company'
 
@@ -12,35 +14,50 @@ const styles = theme => ({
     overflowX: 'auto',
   },
   table: {
-    minWidth: 700,
+    borderTop: '1px solid #E0E0E0',
+    width: '100%',
   },
   tableRow: {
-    height: 175,
+    height: 150,
+  },
+  avatarCell: {
+    width: 75,
+  },
+  avatar: {
+    height: 80,
+    width: 80,
   }
 });
 
-function CompanyTableView(props) {
-  const { classes, companies } = props
+class CompanyTableView extends Component {
 
-  return (
-    <Table className={classes.table}>
-      <TableBody>
-        {companies.map(n => {
-          return (
-            <TableRow key={n.id} className={classes.tableRow}>
-              <TableCell>
-                <Avatar src={n.logo_img_url + '?size=150&format=png'} sizes={200}/>
-              </TableCell>
-              <TableCell>
-                <div>{n.name}</div>
-                <div>{`${n.url} / ${n.city}, ${n.state}`}</div>
-              </TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
-  )
+  handleCompanyClick(event, company) {
+    this.props.history.push(`/industry/${company.industry.id}/company/${company.id}`)
+  }
+
+  render() {
+    const { classes, companies } = this.props
+    
+    return (
+      <Table className={classes.table}>
+        <TableBody>
+          {companies.map(n => {
+            return (
+              <TableRow key={n.id} onClick={event => this.handleCompanyClick(event, n)} className={classes.tableRow}>
+                <TableCell className={classes.avatarCell}>
+                  <Avatar src={n.logo_img_url + '?size=150&format=png'} className={classes.avatar}/>
+                </TableCell>
+                <TableCell>
+                  <h2>{n.name}</h2>
+                  <div>{`${n.url} / ${n.city}, ${n.state}`}</div>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    )
+  }
 }
 
 CompanyTableView.propTypes = {
@@ -48,4 +65,4 @@ CompanyTableView.propTypes = {
   classes: PropTypes.object.isRequired,
 }
 
-export default withStyles(styles)(CompanyTableView);
+export default withStyles(styles)(withRouter(CompanyTableView));
